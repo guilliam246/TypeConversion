225cd8873a2934822c422ff4a337994597152011
guill <guill@GO-21-0911941-2>
2022-05-23 09:34:04 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "True";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number cant be converted to a byte");
+            }
+
+
+         }
+
+            
+     }
+
+ }
+
